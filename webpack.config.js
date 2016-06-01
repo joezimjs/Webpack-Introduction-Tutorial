ddfe4791872cd87110e702d1fedc51ba666b1c6c
Joseph Zimmerman <contact@joezimjs.com>
2016-06-01 16:10:54 -0500

    Tried using Babel Loader... but it's not transforming the ES2015

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: './dist',
         filename: 'bundle.js'
     },
+<<<<<<< HEAD
     module: {
         loaders: [
             {
@@ -28,4 +29,25 @@ module.exports = {
             }
         ]
     }
+=======
+    loaders: [
+        {
+            test: /\.jsx?$/,
+            loader: 'babel',
+            // blacklist node_modules scripts from being processed via Babel
+            exclude: /node_modules/,
+            // or you can use a whitelist:
+            // include: [ path.resolve(__dirname, "src") ],
+
+            // pass in options to babel loader
+            query: {
+                // prevents multiple versions of e.g. regeneratorRuntime from
+                // being created when using generators in multiple files
+                plugins: ['transform-runtime'],
+                // tell Babel which presets to use
+                presets: ['es2015']
+            }
+        }
+    ]
+>>>>>>> Tried using Babel Loader... but it's not transforming the ES2015
 };
