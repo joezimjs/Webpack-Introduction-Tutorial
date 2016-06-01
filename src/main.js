4d0884a26e31dc29890cc5849cbd279791049cb0
Joseph Zimmerman <contact@joezimjs.com>
2016-06-01 16:10:54 -0500

    Tried using Babel Loader... but it's not transforming the ES2015

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-var map = require('lodash/map');
+import { map } from 'lodash';
 
-function square(n) {
-    return n*n;
-}
-
-console.log(map([1,2,3,4,5,6], square));
+console.log(map([1,2,3,4,5,6], n => n*n));
