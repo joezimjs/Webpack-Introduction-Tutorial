37c9c4ea68f75080753cfd4bb37f8c1c278913f4
Joseph Zimmerman <contact@joezimjs.com>
2016-06-17 12:36:46 -0500

    Fixed Webpack configuration issue; Babel works now

--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,25 @@ module.exports = {
         path: './dist',
         filename: 'bundle.js'
     },
-    loaders: [
-        {
-            test: /\.jsx?$/,
-            loader: 'babel',
-            // blacklist node_modules scripts from being processed via Babel
-            exclude: /node_modules/,
-            // or you can use a whitelist:
-            // include: [ path.resolve(__dirname, "src") ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                // blacklist node_modules scripts from being processed via Babel
+                exclude: /node_modules/,
+                // or you can use a whitelist:
+                // include: [ path.resolve(__dirname, "src") ],
 
-            // pass in options to babel loader
-            query: {
-                // prevents multiple versions of e.g. regeneratorRuntime from
-                // being created when using generators in multiple files
-                plugins: ['transform-runtime'],
-                // tell Babel which presets to use
-                presets: ['es2015']
+                // pass in options to babel loader
+                query: {
+                    // prevents multiple versions of e.g. regeneratorRuntime from
+                    // being created when using generators in multiple files
+                    plugins: ['transform-runtime'],
+                    // tell Babel which presets to use
+                    presets: ['es2015']
+                }
             }
-        }
-    ]
+        ]
+    }
 };
