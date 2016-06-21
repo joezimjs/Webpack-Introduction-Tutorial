67c2c520b633574fbf1facc8da5c535324dcd886
Joseph Zimmerman <contact@joezimjs.com>
2016-06-21 16:09:07 -0500

    Compress the Webpack config a bit by removing superfluous comments and new lines

--- webpack.config.js
+++ webpack.config.js
@@ -11,26 +11,10 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.jsx?$/,
-                loader: 'babel',
-                // blacklist node_modules scripts from being processed via Babel
-                exclude: /node_modules/,
-                // or you can use a whitelist:
-                // include: [ path.resolve(__dirname, "src") ],
-
-                // pass in options to babel loader
-                query: {
-                    // prevents multiple versions of e.g. regeneratorRuntime from
-                    // being created when using generators in multiple files
-                    plugins: ['transform-runtime'],
-                    // tell Babel which presets to use
-                    presets: ['es2015']
-                }
+                test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/,
+                query: { plugins: ['transform-runtime'], presets: ['es2015'] }
             },
-            {
-                test: /\.hbs$/,
-                loader: 'handlebars'
-            }
+            { test: /\.hbs$/, loader: 'handlebars' }
         ]
     }
 =======
