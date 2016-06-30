662bb734d1eb78876edc47870ab290c085ac1baa
Joseph Zimmerman <contact@joezimjs.com>
2016-06-30 16:21:31 -0500

    Example 7: Lazy load modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { map } from 'lodash';
-import template from './numberlist.hbs';
 
 let numbers = map([1,2,3,4,5,6], n => n*n);
-document.getElementById("app-container").innerHTML = template({numbers});
+
+setTimeout( () => {
+    require(['./numberlist.hbs'], template => {
+        document.getElementById("app-container").innerHTML = template({numbers});
+    })
+}, 2000);
