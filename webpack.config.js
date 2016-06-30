2c109b8fe6ef313f9ef1250b8ec417f02b1f81b8
Joseph Zimmerman <contact@joezimjs.com>
2016-06-30 16:03:48 -0500

    Example 6: Minify JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+// or
+// var webpack = require('webpack');
 
 module.exports = {
     entry: [
@@ -22,6 +25,14 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Intro to Webpack',
             template: 'src/index.html'
+        }),
+        new UglifyJsPlugin({
+        // or
+        // new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: { screw_ie8 : true },
+            compress: { screw_ie8: true, warnings: false },
+            comments: false
         })
     ]
 };
