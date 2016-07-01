c3306f39b4e0247d410d5875c3d71adbe6728904
Joseph Zimmerman <contact@joezimjs.com>
2016-07-01 09:47:27 -0500

    Example 8: Vendor Chunk

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
-// or
-// var webpack = require('webpack');
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
-    entry: [
-        'babel-polyfill',
-        './src/main.js'
-    ],
+    entry: {
+        vendor: ['babel-polyfill', 'lodash'],
+        main: './src/main.js'
+    },
     output: {
         path: './dist',
         filename: 'bundle.js'
@@ -27,12 +26,11 @@ module.exports = {
             template: 'src/index.html'
         }),
         new UglifyJsPlugin({
-        // or
-        // new webpack.optimize.UglifyJsPlugin({
             beautify: false,
             mangle: { screw_ie8 : true },
             compress: { screw_ie8: true, warnings: false },
             comments: false
-        })
+        }),
+        new CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
     ]
 };
