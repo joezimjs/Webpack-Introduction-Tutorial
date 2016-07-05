11c08214b7a3fc8fddf9d8bf3b72e5409467c295
Joseph Zimmerman <contact@joezimjs.com>
2016-07-05 09:41:49 -0500

    Fixing rebase issues

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
         path: './dist',
         filename: 'bundle.js'
     },
-<<<<<<< HEAD
     module: {
         loaders: [
             {
@@ -17,25 +16,4 @@ module.exports = {
             { test: /\.hbs$/, loader: 'handlebars' }
         ]
     }
-=======
-    loaders: [
-        {
-            test: /\.jsx?$/,
-            loader: 'babel',
-            // blacklist node_modules scripts from being processed via Babel
-            exclude: /node_modules/,
-            // or you can use a whitelist:
-            // include: [ path.resolve(__dirname, "src") ],
-
-            // pass in options to babel loader
-            query: {
-                // prevents multiple versions of e.g. regeneratorRuntime from
-                // being created when using generators in multiple files
-                plugins: ['transform-runtime'],
-                // tell Babel which presets to use
-                presets: ['es2015']
-            }
-        }
-    ]
->>>>>>> Tried using Babel Loader... but it's not transforming the ES2015
 };
